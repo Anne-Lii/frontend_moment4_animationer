51a18160203925772327b9b5e3287b4a47821ce6
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-02-21 10:14:00 +0100

    fått kartan att fungera med sökning

--- src/JS/map.js
+++ src/JS/map.js
@@ -1,6 +1,11 @@
 /* kod skriven av Anne-Lii Hansen anha2324@student.miun.se*/
 "use strict"
 
+//hämta element från HTML-koden
+const searchButtonEl = document.getElementById("searchButton");
+const searchFieldEl = document.getElementById("searchField");
+const mapEl = document.getElementById("map");
+
 let map = L.map('map').setView([58.03222280237399, 14.349985267448993], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,92 +13,41 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//placera markör
 let marker = L.marker([58.03222280237399, 14.349985267448993]).addTo(map);
 
-/* GOOGLE MAPS
-//hämta element från HTML-koden
-const searchButtonEl = document.getElementById("searchButton");
-const searchFieldEl = document.getElementById("searchField");
-const mapEl = document.getElementById("map");
+//Locate controle
+L.control.locate().addTo(map);
+
 
 //event vid klick på SÖK-knappen
-searchButtonEl.onclick = function () {   
-    let url = "";
-
+searchButtonEl.addEventListener("click", function() {   
+    
     let searchInput = searchFieldEl.value;//input från sökfältet
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${searchInput}`;
 
-    fetch(url + searchInput) //hämta data från API plus input från sökfältet
+  
+
+    fetch(url) //hämta data från API plus input från sökfältet
         .then(response => response.json())//konvertera till javascript
         .then(data => {//ger mig data tillbaka
-
+      
             //plocka ut koorinaterna från sökningen
             let lat = data[0].lat;
             let lon = data[0].lon;
             
-            console.log(lat);
-            console.log(lon);
+            //uppdatera kartan med nya koordinater och zoom
+            map.setView([lat, lon], 12);
 
-            //skicka iväg url plus koordinater för Openstreetmap
-            let mapURL = `https://www.openstreetmap.org/export/embed.html?bbox=${lon},${lat},${lon},${lat}&layer=mapnik`;
-            console.log(mapURL);
+            // Placera markören på de nya koordinaterna
+            marker.setLatLng([lat, lon]);       
+
             //uppdatera kartan med nya koordinater
-            mapEl.src = mapURL;
+            mapEl.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon},${lat},${lon},${lat}&layer=mapnik`;
         })
 
         //fånga error
         .catch(error => {
             console.error("det har uppstått ett fel", error);
         })
-};
-*/
-
-
-
-
-
-
-
-
-/* openstreetmap men utan markörer
-
-//hämta element från HTML-koden
-const searchButtonEl = document.getElementById("searchButton");
-const searchFieldEl = document.getElementById("searchField");
-const mapEl = document.getElementById("map");
-const url = "https://nominatim.openstreetmap.org/search?format=json&q=";
-
-window.onload = function(){
-   
-    let mapURL = "https://www.openstreetmap.org/export/embed.html?bbox=14.3515176,58.0321418,14.3515176,58.0321418&layer=mapnik";
-    mapEl.src = mapURL;
-};
-
-//event vid klick på SÖK-knappen
-searchButtonEl.onclick = function () {    
-
-    let searchInput = searchFieldEl.value;//input från sökfältet
-
-    fetch(url + searchInput) //hämta data från API plus input från sökfältet
-        .then(response => response.json())//konvertera till javascript
-        .then(data => {//ger mig data tillbaka
-
-            //plocka ut koorinaterna från sökningen
-            let lat = data[0].lat;
-            let lon = data[0].lon;
-            
-            console.log(lat);
-            console.log(lon);
-
-            //skicka iväg url plus koordinater för Openstreetmap
-            let mapURL = `https://www.openstreetmap.org/export/embed.html?bbox=${lon},${lat},${lon},${lat}&layer=mapnik`;
-            console.log(mapURL);
-            //uppdatera kartan med nya koordinater
-            mapEl.src = mapURL;
-        })
-
-        //fånga error
-        .catch(error => {
-            console.error("det har uppstått ett fel", error);
-        })
-};
-*/
+});
